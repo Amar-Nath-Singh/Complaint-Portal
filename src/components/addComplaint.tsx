ccbfec8b984f2c75d10b8165aedcadd85ec15d83
Amar-Nath-Singh <amarnathsingh.iitm@gmail.com>
2022-09-04 11:03:57 +0530

    Zz

--- src/components/addComplaint.tsx
+++ src/components/addComplaint.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Component } from 'react';
-import axios from 'axios'
+import axios from 'axios';
+import './addcomplaint.css';
 interface AddComplaintProps {
 
 }
@@ -10,20 +11,24 @@ interface AddComplaintState {
 }
 
 class AddComplaint extends React.Component<AddComplaintProps, AddComplaintState> {
-    state = {status:"pending",}
+    state = { status: "pending", name: "", rollno: "", desp: "" }
+    visi = false
     handleClick = (e: React.MouseEvent<HTMLElement>) => {
         axios
             .post("http://localhost:5000/dataset/addcomplaint", this.state)
             .then((res) => console.log(res.data));
-        
+        this.visi = true
     }
     render() {
         return (
             <div className='add-complaint'>
-                <input placeholder='Name' onChange={event => this.setState({ name: event.target.value })}></input>
-                <input placeholder='rollno' onChange={event => this.setState({ rollno: event.target.value })}></input>
-                <input placeholder='desp' onChange={event => this.setState({ desp: event.target.value })}></input>
-                <button onClick={this.handleClick}>Submit</button>
+                <input type="text" onClick={(e)=> this.visi = false} placeholder='Name' onChange={event => this.setState({ name: event.target.value })}></input>
+                <input type="text" onClick={(e)=> this.visi = false} placeholder='rollno' onChange={event => this.setState({ rollno: event.target.value })}></input>
+                <input type="text" onClick={(e)=> this.visi = false} placeholder='desp' onChange={event => this.setState({ desp: event.target.value })}></input>
+                <button className="btn" onClick={this.handleClick}>Submit</button>
+                <a className='Add-info' style={{ visibility: this.visi ? "visible" : "hidden" }}>
+                    Data Added.
+                </a>
             </div>
         );
     }
