5d9a587b6644051e9489fb908b3bbd49b6d8692c
Amar-Nath-Singh <amarnathsingh.iitm@gmail.com>
2022-09-04 10:19:53 +0530

    Zzzzzzzzz

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -28,7 +28,7 @@ class Dashboard extends React.Component<DashboardProps, DashboardState> {
     render() {
         return (
             <div className='dashboard-box'>
-                <p className="dashboard-head">Heading</p>
+                <p className="dashboard-head"></p>
                 <div className='complaint-box'>
                     <div className='complaint-strip'>
                         {this.props.complaints.map((complaint) =>
